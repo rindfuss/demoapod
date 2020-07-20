f17cf4262ca32e16c64ad7fd80bda94a559f7b6d
Rich Rindfuss <rich@CalmSeasSoftware.com>
2020-07-20 17:01:25 -0500

    recreated build folder
    recreated build folder after removing the locally cached version which had been set up as a nested git repo in the past and was causing issues with github and heroku

---
+++ build/precache-manifest.9f3aff0d93243a481b8b91f5a5ec7dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccda0e20aff99844ae9a1090031f912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95cf7f20969d0301356",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ae0c425e9a492181455",
+    "url": "/static/js/2.118624df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.118624df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95cf7f20969d0301356",
+    "url": "/static/js/main.10f6a69f.chunk.js"
+  },
+  {
+    "revision": "5daa678d6ca7173b3a8d",
+    "url": "/static/js/runtime-main.eafa189d.js"
+  }
+]);
