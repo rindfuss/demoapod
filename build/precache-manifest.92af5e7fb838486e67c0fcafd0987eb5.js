c4f31e70d7411d1d812a7e50e7e5c85975b66643
Rich Rindfuss <rich@CalmSeasSoftware.com>
2020-07-20 20:51:36 -0500

    switched to node server
    modified project to use a node server due to error I was receiving when deploying to Heroku using a PHP and either nginx or apache server

---
+++ build/precache-manifest.92af5e7fb838486e67c0fcafd0987eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e59da21ed42b29abc58a52b799ec6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38517bd7c3a6bc28972",
+    "url": "/static/css/main.69c3128f.chunk.css"
+  },
+  {
+    "revision": "72e86fad566a6cd96196",
+    "url": "/static/js/2.88615bce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88615bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38517bd7c3a6bc28972",
+    "url": "/static/js/main.a45c6f4d.chunk.js"
+  },
+  {
+    "revision": "420a557a96859f92db24",
+    "url": "/static/js/runtime-main.7f670f00.js"
+  }
+]);
